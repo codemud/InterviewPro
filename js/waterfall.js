55ff3f82a0288751d69f88f9b66d4a1fc22c43d8
zhangyang <861471707@qq.com>
2017-05-23 23:30:46 +0800

    设置瀑布流布局区域main宽度

---
+++ js/waterfall.js
@@ -0,0 +1,56 @@
+(function() {
+	// // 父级对象
+	// var oParent=document.getElementById('main');
+	// // 获取存储块框pin的数组aPin
+	// var aPin=getClassObj(oParent,pin);
+	// //获取-每行中能容纳的块框个数-num【窗口宽度除以一个块框宽度】
+	// var num=Math.floor(document.documentElement.clientWidth/aPin[0].offsetWidth);
+	// //用cssText属性为父级main添加居中样式：定宽+自动水平外边距
+	// oParent.style.cssText= 'width:' +iPinW*num+ 'px;margin:0 auto;';
+	// function getClassObj(parent,className){
+ //       var obj=parent.getElementsByTagName( '*' ); //获取 父级的所有子集
+ //       var pinS=[]; //创建一个数组 用于存储类为className的元素
+ //       for( var   i=0;i<obj.length;i++) { //遍历子集、判断类名、压入数组
+ //           if(obj[i].className==className)
+ //               pinS.push(obj[i]);
+ //       };
+ //       return pinS;
+	//  }
+	
+		window.onload=function(){  
+             changeDivHeight();  
+        }  
+        //当浏览器窗口大小改变时，设置显示内容的高度  
+        window.onresize=function(){  
+             changeDivHeight();  
+        }  
+        function changeDivHeight(){               
+            var w = document.documentElement.clientWidth;//获取页面可见高度  
+            var boxW = w/100;
+            console.log(w)
+            document.getElementById("main").style.width=w-(boxW*39)+"px";
+            console.log(w-(boxW*39)+".....")  
+		}
+
+	    // //每行中能容纳的展示框个数【窗口宽度除以一个块框宽度】
+	    // var cols = Math.floor(($( window ).width()-30) / w );
+	    // //给最外围的main元素设置宽度和外边距
+	    // $('#main').width(w*cols).css('margin','o auto');
+	    // //用于存储 每列中的所有块框相加的高度。
+	    // var hArr=[];
+	    // $boxs.each( function( index, value ){
+	    //     var h = $boxs.eq( index).outerHeight();
+	    //     if( index < cols ){
+	    //         hArr[ index ] = h; //第一行中的num个块框 先添加进数组HArr
+	    //     }else{
+	    //         var minH = Math.min.apply( null, hArr );//数组HArr中的最小值minH
+	    //         var minHIndex = $.inArray( minH, hArr );
+	    //         $( value).css({
+	    //             'position':'absolute','top':minH+'px', 'left':minHIndex*w + 'px'
+	    //         });
+	    //         //数组 最小高元素的高 + 添加上的展示框[i]块框高
+	    //         hArr[ minHIndex ] += $boxs.eq( index).outerHeight();//更新添加了块框后的列高
+	    //     }
+	    // });
+
+})();
