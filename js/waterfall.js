9e5696b0fb66f77f80f255b80414a3db538e58ac
zhangyang <861471707@qq.com>
2017-05-27 00:09:25 +0800

    获取到瀑布流布局区域中li数组

--- js/waterfall.js
+++ js/waterfall.js
@@ -16,9 +16,10 @@
  //       };
  //       return pinS;
 	//  }
-		var windowWidth;
+
+		var windowWidth,liarr;
 		var w = 250;
-		var w = document.documentElement.clientWidth;//获取页面可见高度 
+		var w = document.documentElement.clientWidth;//获取页面可见宽度
 		var boxW = Math.floor(w/100);//每1%所指定宽度
 		window.onload=function(){  
              changeDivHeight();  
@@ -27,26 +28,69 @@
         window.onresize=function(){  
              changeDivHeight();  
         }  
-
+        //页面改变时，计算当前瀑布流实时宽度
         function changeDivHeight(){ 
             // var boxW = Math.floor(w/100);
             // console.log(w)
             document.getElementById("main").style.width=w-(boxW*39)+"px";
-            windowWidth = w-(boxW*39);
+            windowWidth = w-(boxW*39);            
             console.log(windowWidth)
 		}
 
-		//每行中能容纳的展示框个数【窗口宽度除以一个块框宽度】
+		//每行中能容纳的展示框个数[窗口宽度除以一个块框宽度]
 		var cols = Math.floor((w-(boxW*39))/250);
 		console.log(cols)
-		//给ul设置定位为relative，下面的每个li设置为absolute，
-		//  然后去掉margin-bottom与margin-right，然后通过设置left与top来控制每个的li位置
-
+		//给ul设置定位为relative，下面的每个li设置为absolute，然后去掉margin-bottom与margin-right，然后通过设置left与top来控制每个的li位置
+	
 		//1.获取到ul下所有的li，然后给对应的li添加样式
-
 		
-		
-
+		//这里会出现一个问题，会报Cannot read property 'getElementsByTagName' of null错，
+		// 原因是因为页面加载的顺序，index.html出现，会先加载head里面的各个js引入js文件，这就会存在
+		// 加载顺序的一个优先级
+		liarr= document.getElementById("picList").getElementsByTagName("li");
+		console.log(liarr)
+		//2.使用循环给各个li设置齐top与left
+		function waterfall(){
+			var hArr;
+			for(var i = 0;i < liarr.length;i++){  
+		        if(i<cols){  
+		            hArr.push(liarr[i].offsetHeight);  
+		        }else{  
+		            var minH = Math.min.apply(null,hArr);  
+		            console.log(minH);  
+		            var index= hArr.indexOf(minH);  
+		            console.log(index);  
+		            liarr[i].style.position = 'absolute';  
+		            liarr[i].style.top = minH +'px';  
+		            liarr[i].style.left = index*cols + 'px';  
+		            hArr[index] +=liarr[i].offsetHeight;
+		        }  
+			}  
+			console.log(hArr); 
+		}
+		 
+		// window.onload = function(){  
+		//     waterfall('main','box');  
+		//     var dataInt = {"data":[{"src":"48.jpg"},{"src":"49.jpg"},{"src":"50.jpg"},{"src":"51.jpg"},{"src":"52.jpg"},{"src":"53.jpg"},{"src":"54.jpg"}]}  
+		//     window.onscroll = function(){  
+		//         var oParent = document.getElementById('main');  
+		//         if(checkscrollside){  
+		//             //将数据块渲染到页面中尾部  
+		//             for(var i = 0;i<dataInt.data.length;i++){  
+		//                 var oBox = document.createElement('div');  
+		//                 oBox.className = 'box';  
+		//                 oParent.appendChild(oBox);  
+		//                 var oPic = document.createElement('div');  
+		//                 oPic.className = 'pic';  
+		//                 oBox.appendChild(oPic);  
+		//                 var oImg = document.createElement('img');  
+		//                 oImg.src = 'img/'+ dataInt.data[i].src;  
+		//                 oPic.appendChild(oImg);  
+		//             }  
+		//             waterfall('main','box'); 		              
+		//         }  		  
+		//     }  		  
+		// }  
 		//取得展示框对象
     	// var $boxs = $("#main>div");
 	    // //给最外围的main元素设置宽度和外边距
